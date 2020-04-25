9deae5c242841682ca950a56abe116ca5412ca48
Muhamad Fajar <mfajar0910@gmail.com>
2020-04-26 01:43:04 +0700

    Fix translation content and add toc toggle setting

--- themes/hello-friend-ng-fork/assets/js/theme.js
+++ themes/hello-friend-ng-fork/assets/js/theme.js
@@ -18,72 +18,109 @@ const fontSizeIncrease = document.querySelector(".increase-font-size");
 const fontSizeDecrease = document.querySelector(".decrease-font-size");
 let fontSize = parseFloat(getFontSize) || 0.85;
 
+// TOC
+const getToc = window.localStorage && window.localStorage.getItem("toc");
+const toc = document.querySelector(".toc");
+const tocToggle = document.querySelector(".toc-toggle");
+const tocToggleText = document.querySelector("#toc-toggle-button");
+const tocIsHide = getToc === "hide";
 
 const invertImgColor = () => {
-  if (homepageImage) {
-    document.body.classList.contains("dark-theme") ? homepageImage.src = "/assets/img/light.png" : homepageImage.src = "/assets/img/dark.png";
-  }
-  if (notFoundImage) {
-    document.body.classList.contains("dark-theme") ? notFoundImage.src = "/assets/img/404-light.png" : notFoundImage.src = "/assets/img/404-dark.png";
-  }
+    if (homepageImage) {
+        document.body.classList.contains("dark-theme") ? homepageImage.src = "/assets/img/light.png" : homepageImage.src = "/assets/img/dark.png";
+    }
+    if (notFoundImage) {
+        document.body.classList.contains("dark-theme") ? notFoundImage.src = "/assets/img/404-light.png" : notFoundImage.src = "/assets/img/404-dark.png";
+    }
 };
 
 if (getTheme !== null) {
-  document.body.classList.toggle("dark-theme", isDark);
-  if (isDark) {
-    metaThemeColor.setAttribute("content", "#252627");
-    themeToggleText.textContent = buttonTextDark;
-    // invertImgColor();
-  } else {
-    metaThemeColor.setAttribute("content", "#fafafa");
-    themeToggleText.textContent = buttonTextLight;
-    // invertImgColor();
-  }
+    document.body.classList.toggle("dark-theme", isDark);
+    if (isDark) {
+        metaThemeColor.setAttribute("content", "#252627");
+        if (window.location.href.indexOf("settings") > -1) {
+            themeToggleText.textContent = buttonTextDark;
+        }
+        // invertImgColor();
+    } else {
+        metaThemeColor.setAttribute("content", "#fafafa");
+        if (window.location.href.indexOf("settings") > -1) {
+            themeToggleText.textContent = buttonTextLight;
+        }
+        // invertImgColor();
+    }
 }
 
-if (getFont !== null ) {
-  document.body.classList.toggle("normal-font", isNormal);
+if (getFont !== null) {
+    document.body.classList.toggle("normal-font", isNormal);
 }
 
-if (getFontSize !== null ) {
-  document.body.style.fontSize = getFontSize + 'rem';
+if (getFontSize !== null) {
+    document.body.style.fontSize = getFontSize + 'rem';
 }
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-  invertImgColor();
-  window.localStorage && window.localStorage.setItem(
-      "theme",
-      document.body.classList.contains("dark-theme") ? "dark" : "light",
-    );
-  document.body.classList.contains("dark-theme") ? metaThemeColor.setAttribute("content", "#252627") : metaThemeColor.setAttribute("content", "#fafafa");
-  document.body.classList.contains("dark-theme") ? themeToggleText.textContent = buttonTextDark : themeToggleText.textContent = buttonTextLight;
-});
+if (getToc !== null) {
+    document.body.classList.toggle("hide-toc", tocIsHide);
+    if (tocIsHide) {
+        if (document.getElementById("toc-container")) {
+            document.getElementById("toc-container").style.display = "none"
+        }
+        if (window.location.href.indexOf("settings") > -1) {
+            tocToggleText.textContent = buttonTextTocHide;
+        }
+    } else {
+        if (window.location.href.indexOf("settings") > -1) {
+            tocToggleText.textContent = buttonTextTocShow;
+        }
+    }
+}
 
-fontToggle.addEventListener("click", () => {
-  document.body.classList.toggle("normal-font");
-  window.localStorage && window.localStorage.setItem(
-      "font",
-      document.body.classList.contains("normal-font") ? "normal" : "mono",
-    );
-});
+if (window.location.href.indexOf("settings") > -1) {
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-theme");
+        invertImgColor();
+        window.localStorage && window.localStorage.setItem(
+            "theme",
+            document.body.classList.contains("dark-theme") ? "dark" : "light",
+        );
+        document.body.classList.contains("dark-theme") ? metaThemeColor.setAttribute("content", "#252627") : metaThemeColor.setAttribute("content", "#fafafa");
+        document.body.classList.contains("dark-theme") ? themeToggleText.textContent = buttonTextDark : themeToggleText.textContent = buttonTextLight;
+    });
 
-fontSizeIncrease.addEventListener("click", () => {
-  fontSize += 0.05;
-  changeFontSize(fontSize)
-});
+    fontToggle.addEventListener("click", () => {
+        document.body.classList.toggle("normal-font");
+        window.localStorage && window.localStorage.setItem(
+            "font",
+            document.body.classList.contains("normal-font") ? "normal" : "mono",
+        );
+    });
 
-fontSizeDecrease.addEventListener("click", () => {
-  fontSize -= 0.05;
-  changeFontSize(fontSize)
-});
+    fontSizeIncrease.addEventListener("click", () => {
+        fontSize += 0.05;
+        changeFontSize(fontSize)
+    });
+
+    fontSizeDecrease.addEventListener("click", () => {
+        fontSize -= 0.05;
+        changeFontSize(fontSize)
+    });
+
+    tocToggle.addEventListener("click", () => {
+        document.body.classList.toggle("hide-toc");
+        window.localStorage && window.localStorage.setItem(
+            "toc",
+            document.body.classList.contains("hide-toc") ? "hide" : "show",
+        );
+        document.body.classList.contains("hide-toc") ? tocToggleText.textContent = buttonTextTocHide : tocToggleText.textContent = buttonTextTocShow;
+    });
+}
 
 function changeFontSize(fontSize) {
-  if (fontSize >= 0.85 && fontSize <= 1.85) {
-    window.localStorage && window.localStorage.setItem(
-        "font-size",
-        String(fontSize),
-    );
-    document.body.style.fontSize = fontSize + 'rem';
-  }
+    if (fontSize >= 0.85 && fontSize <= 1.85) {
+        window.localStorage && window.localStorage.setItem(
+            "font-size",
+            String(fontSize),
+        );
+        document.body.style.fontSize = fontSize + 'rem';
+    }
 }
